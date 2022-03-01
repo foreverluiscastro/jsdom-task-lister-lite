fab00a7ce23c44b287ec07f9dcd6201e05cb6adf
luis <foreverluiscastro@gmail.com>
2022-03-01 12:55:19 -0500

    TC support session

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newTaskForm = document.querySelector("form")
+  newTaskForm.addEventListener("submit", createNewTask);
 });
+
+function createNewTask() {
+  const newTaskDescription = document.querySelector("input");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+  document.querySelector("tasks").appendChild(task);
+};
